31591a2e806b34c404965272fe9e4f4906579e2a
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-02-26 18:13:16 +0200

    ADDED Basic styling with tail wind

--- astroJSBlog/astro.config.mjs
+++ astroJSBlog/astro.config.mjs
@@ -5,6 +5,11 @@ import sanity from 'astro-sanity';
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   markdown: {
     smartypants: false,
@@ -15,5 +20,5 @@ export default defineConfig({
     dataset: 'production',
     apiVersion: '2021-03-25',
     useCdn: true
-  }),]
-},[react()]);
+  }), [react()], tailwind()]
+});
