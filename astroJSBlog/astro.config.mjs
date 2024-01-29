6f3a0bfdb13cf552c52e8d5c9b78ecd9832a055b
omarashzeinhom <omar.ashraf.zeinhom@Hotmail.com>
2024-01-29 02:09:45 +0000

    replaced plugin for deployment errors

--- astroJSBlog/astro.config.mjs
+++ astroJSBlog/astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sanity from 'astro-sanity';
-import markdownIntegration from '@astropub/md'
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-import { createMarkdownProcessor } from '@astrojs/markdown-remark'; // Import the correct export
 
 
 export default defineConfig({
-  vite: {
-    ssr: {
-      // Add npm packages containing invalid code here
-      noExternal: ["@astrojs/markdown-remark","@astropub/md"],
-    },
-  },
+  
   integrations: [
     sanity({
       projectId: 'tojmg0db',
@@ -22,7 +15,6 @@ export default defineConfig({
     }),
     [react()],
     tailwind(),
-    markdownIntegration(),
   ],
   markdown: {
     smartypants: false,
@@ -30,7 +22,6 @@ export default defineConfig({
     remarkPlugins: [],
     rehypePlugins: [],
     //syntaxHighlight: 'shiki',
-    createMarkdownProcessor, // Use the correct export
   },
   components: {
     types: {
